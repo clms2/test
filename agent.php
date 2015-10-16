4783a9e3761b5a8e02366fb29575151cdf3540a4
clms2 <18067087965@163.com>
2015-10-16 17:59:50 +0800

    a
    a

---
+++ agent.php
@@ -0,0 +1,23 @@
+<?php
+// 首先访问一下设置cookie
+if(isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], 'test.com') !== false){
+	header('P3P: CP="CURa ADMa DEVa PSAo PSDo OUR BUS UNI PUR INT DEM STA PRE COM NAV OTC NOI DSP COR"');   
+	setcookie("test", 'test', time()+1800, "/");
+	exit('1');
+}
+// 验证
+if(empty($_COOKIE['test'])){
+	exit('500');
+}
+
+
+$url = isset($_GET['url']) ? $_GET['url'] : '';
+
+if(!empty($url)){
+	$urlarr = parse_url($url);
+	$domain = $urlarr['host'];
+	$ct = file_get_contents($url);
+	$ct = str_replace($domain, '', $ct);
+	$js = '<script>(function(){if(typeof jQuery != "function" ) return;$("a").click(function(){});})();</script>';
+
+}
