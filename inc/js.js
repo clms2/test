b3e7cded3b33f07467f4424dd6797fcd185e493d
clms2 <18067087965@163.com>
2016-03-28 15:23:57 +0800

    Update js.js

--- inc/js.js
+++ inc/js.js
@@ -13,6 +13,22 @@ function strtr(str, assoc){
 }
 
 /**
+ * php substr_count的js版
+ * @param  {string} str    
+ * @param  {string} search 
+ * @param  {int} total  不需要传该参数
+ * @return {int}        
+ */
+function substr_count(str, search, total){
+    var total = total || 0,
+        index = str.indexOf(search);
+
+    if(index === -1) return total;
+
+    return substr_count(str.substr(++index), search, ++total);
+}
+
+/**
  * 返回2015-11:03 11:20
  * @param  {object} obj new Date()的对象
  * @return {string}     
