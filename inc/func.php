4c57ec0ea07d946bc99d7f37d53999b906368449
clms2 <18067087965@163.com>
2016-07-19 11:52:28 +0800

    Update func.php

--- inc/func.php
+++ inc/func.php
@@ -1,4 +1,5 @@
 <?php 
+
 /**
  * array_key_exists的递归搜索
  * @param  string $key   键名
@@ -7,24 +8,24 @@
  * @return string/boolean  
  */
 function array_key_exists_deep($key, $arr, $field = null){
-	if(array_key_exists($key, $arr)){
-		if(isset($field)){
-			return $arr[$key][$field];
-		}
-		return true;
-	}
-	foreach ($arr as $k => $a) {
-		$find = false;
-		if(is_array($a)){
-			$r = $this->array_key_exists_deep($key, $a, $field);
-			if($r){
-				return $r;
-			}
-		}
-	}
+    if(array_key_exists($key, $arr)){
+        if(isset($field)){
+            return $arr[$key][$field];
+        }
+        return true;
+    }
+    foreach ($arr as $a) {
+        if(is_array($a)){
+            $r = array_key_exists_deep($key, $a, $field);
+            if($r){
+                return $r;
+            }
+        }
+    }
 
-	return false;
+    return false;
 }
+
 
 function strToBin($str){
 	$arr = preg_split('/(?<!^)(?!$)/u', $str);
